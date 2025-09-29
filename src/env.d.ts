459ecab4b44a8fee697a82c40374307ce9b49f42
Moretti-Federico <84404137+Moretti-Federico@users.noreply.github.com>
2025-09-29 18:33:40 +0100

    Update env.d.ts

--- src/env.d.ts
+++ src/env.d.ts
@@ -1,6 +1,6 @@
 /// <reference types="astro/client" />
 interface ImportMetaEnv {
-  readonly WP_GRAPHQL_ENDPOINT: string
+  readonly WP_GRAPHQL_ENDPOINT: "https://spuntoai.instawp.xyz/graphql"
 }
 interface ImportMeta {
   readonly env: ImportMetaEnv
